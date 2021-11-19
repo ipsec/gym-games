96b2840fe46d6b3a06c65c446a4a07c12e19b3f0
Fernando Ribeiro da Silva <fernando@ribeiro.eng.br>
2021-11-19 00:08:39 -0300

    Using observation_space between -1 and 1

--- gym_games/envs/foo_env.py
+++ gym_games/envs/foo_env.py
@@ -9,7 +9,7 @@ class FooEnv(gym.Env):
 
     def __init__(self):
         self.count = 0
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
         self.action_space = spaces.Box(low=0, high=20, shape=(1,), dtype=np.int64)
 
     def step(self, action):
