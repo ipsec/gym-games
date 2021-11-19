2243d1cb01b34b67dae9e7c2572f1f0a3c594edc
Fernando Ribeiro da Silva <fernando@ribeiro.eng.br>
2021-11-19 00:21:51 -0300

    Testing object

--- gym_games/envs/foo_env.py
+++ gym_games/envs/foo_env.py
@@ -4,11 +4,17 @@ import numpy as np
 from gym import spaces
 
 
+class TestObject:
+    def __init__(self):
+        self.name = 'test'
+
+
 class FooEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         self.count = 0
+        self.test_obj = None
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32)
         self.action_space = spaces.Box(low=5, high=20, shape=(1,), dtype=np.float32)
 
@@ -23,6 +29,7 @@ class FooEnv(gym.Env):
 
     def reset(self):
         self.count = 0
+        self.test_obj = TestObject()
         return np.array([0.], dtype=np.float32)
 
     def render(self, mode="human"):
